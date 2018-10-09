3a41b49507ba174083202b256a63aaf473af54f4
Sudheer Sammeta <sammetasudheer89@gmail.com>
2018-10-09 02:52:23 -0700

    Add status code to responses

--- server/api/controllers/productsController.js
+++ server/api/controllers/productsController.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose'),
 exports.list_all_products = function(req, res) {
   Product.find({}, function(err, product) {
     if (err)
-      res.send(err);
-    res.json(product);
+      res.status(404).send(err);
+    res.status(200).json(product);
   });
 };
 
@@ -19,24 +19,24 @@ exports.create_a_product = function(req, res) {
   const new_product = new Product(req.body);
   new_product.save(function(err, product) {
     if (err)
-      res.send(err);
-    res.json(product);
+      res.status(400).send(err);
+    res.status(201).json(product);
   });
 };
 
 exports.get_a_product = function(req, res) {
   Product.findById(req.params.productId, function(err, product) {
     if (err)
-      res.send(err);
-    res.json(product);
+      res.status(404).send(err);
+    res.status(200).json(product);
   });
 };
 
 exports.update_a_product = function(req, res) {
   Product.findOneAndUpdate({_id:req.params.productId}, req.body, {new: true}, function(err, product) {
     if (err)
-      res.send(err);
-    res.json([product]);
+      res.status(404).send(err);
+    res.status(200).json(product);
   });
 };
 // Product.remove({}).exec(function(){});
@@ -46,7 +46,7 @@ exports.delete_a_product = function(req, res) {
     _id: req.params.productId
   }, function(err, product) {
     if (err)
-      res.send(err);
-    res.json({ message: 'Product successfully deleted' });
+      res.status(404).send(err);
+    res.status(200).json({ message: 'Product successfully deleted' });
   });
 };
