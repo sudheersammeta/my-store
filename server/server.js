b16c8747ae13bd875036e0190cd7cecc424c46d5
Sudheer Sammeta <sammetasudheer89@gmail.com>
2018-10-09 02:53:45 -0700

    Add CORS package

--- server/server.js
+++ server/server.js
@@ -5,11 +5,17 @@ const express = require('express'),
   Task = require('./api/models/productsModel'),
   bodyParser = require('body-parser');
 
+let cors = require('cors');
 const routes = require('./api/routes/productsRoutes');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/rahidb');
 
+//Enable CORS
+var corsOptions = {
+    origin: 'http://localhost:3000'
+}
+app.use(cors(corsOptions))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
